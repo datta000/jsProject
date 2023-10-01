117ac698cae72f7b76759a0ef1bdf04abcbb55d3
Ritaban Datta <ritabandatta2000@gmail.com>
2023-10-01 11:54:19 +0000

    first commit

---
+++ test/02_array.js
@@ -0,0 +1,13 @@
+const marvel_heros=["ironman","spiderman","thor"]
+const dc_heros=["superman","batman","flash"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// console.log(marvel_heros.concat(dc_heros))
+// console.log(...dc_heros,...marvel_heros)
+// console.log(Array.isArray("ritaban"))
+// console.log(Array.from("ritaban"))
+const a=1
+const b=2
+const c=3
+
+console.log(Array.of(a,b,c))
---
+++ test/array.js
@@ -0,0 +1,15 @@
+const myArr=[0,1,2,3,4,5];
+const myArr2= new Array(2,3,4,5);
+
+//methods
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(9)
+// console.log(myArr.includes(3))
+// console.log(myArr.indexOf(4))
+// console.log(myArr.join());
+// console.log(myArr);
+// console.log(myArr)
+// console.log(myArr.slice(1,3), myArr)
+// console.log(myArr.splice(1,3), myArr)
